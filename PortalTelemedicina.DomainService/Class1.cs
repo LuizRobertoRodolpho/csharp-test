fa71a75c93ce1f4c7978c1ab2857a5900237cc93
Luiz Roberto Lethang Rodolpho <luiz.roberto@elune.com.br>
2018-03-24 13:52:33 -0300

    Include domain, repository and test layers.

---
+++ PortalTelemedicina.DomainService/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace PortalTelemedicina.DomainService
+{
+    public class Class1
+    {
+    }
+}
---
+++ PortalTelemedicina.Repository/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace PortalTelemedicina.Repository
+{
+    public class Class1
+    {
+    }
+}
